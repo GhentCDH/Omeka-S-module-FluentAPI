b61fb68d889522d6a25e27fe47f09b1e9d7455d1
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-05-28 09:19:39 +0200

    added resource types as consts

--- src/Model/FieldValue/Entity.php
+++ src/Model/FieldValue/Entity.php
@@ -6,7 +6,12 @@ use FluentAPI\Model\FieldValue;
 
 class Entity extends FieldValue
 {
-    public function __construct(string $term, string $id, string $type = 'resource', ?bool $isPublic = null)
+    public const TYPE_RESOURCE = 'resource';
+    public const TYPE_ITEM = 'resource:item';
+    public const TYPE_ITEM_SET = 'resource:itemset';
+
+
+    public function __construct(string $term, string $id, string $type = self::TYPE_RESOURCE, ?bool $isPublic = null)
     {
         parent::__construct(
             $term,
